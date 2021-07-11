75512a83eee68ef7f21572664d0138b5192b8e50
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-07-11 23:02:20 +0700

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -185,7 +185,7 @@ else:
 DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
 
-REDIS = StrictRedis.from_url(REDIS_URL,decode_responses=True)
+REDIS = StrictRedis.from_url(REDIS_URL,decode_responses=False)
 
 try:
 
